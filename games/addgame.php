83b0a0e0d6d79225d30231af43262695c8877cc3
Connor Bell <connor_bell@verizon.net>
2016-01-26 23:10:23 -0600

    Support for adding games, list of games refreshes inline

--- games/addgame.php
+++ games/addgame.php
@@ -5,3 +5,15 @@
  * Date: 1/18/16
  * Time: 4:58 PM
  */
+require_once('../database/csdb.php');
+header('Content-Type: application/json');
+
+$mysqli = getDB();
+$toInsert = $_POST["name"];
+$query = "INSERT INTO `games` (`name`) VALUES ('" . $toInsert . "')";
+if ($mysqli->query($query) === TRUE) {
+    $status =  "New record created successfully";
+} else {
+    $status =  "Error: " . $query . "<br>" . $mysqli->error;
+}
+echo json_encode(array($status,$toInsert));
--- scripts/graphs/home.js
+++ scripts/graphs/home.js
@@ -2,6 +2,23 @@
  * Created by connorbell on 1/18/16.
  */
 $(document).ready(function(){
+    reloadList();
+
+    $("#newGame").click(function(){
+        console.log($('#newName').val());
+        $.ajax({
+            type: "post",
+            dataType: "json",
+            data:{name:$('#newName').val()},
+            url: '/games/addgame.php',
+            success: function(res){
+                reloadList();
+            }
+        });
+    });
+});
+function reloadList(){
+
     $.ajax({
         dataType: "json",
         url: '/games/listgames.php',
@@ -10,8 +27,8 @@ $(document).ready(function(){
             $.each(res, function(idx, content){
                 htmlToInsert += '<a href="/">' + content["name"] + "</a>";
             });
+            $('#gameslist').empty();
             $('#gameslist').append(htmlToInsert);
         }
     });
-
-});
+}
